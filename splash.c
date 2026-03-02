389a50d3dc2d8bad9ee28bfb654a85e3d21c05ac
Milleniun2133 <mishg.2009@gmail.com>
2026-03-02 22:27:26 +1100

    Changed typo spash. to crrect spelling  splash.

---
+++ splash.c
@@ -0,0 +1,137 @@
+#include "splash.h"
+
+// Forward declerations from kernel.c
+void terminal_putchar(char c);
+void terminal_writestring(const char* data);
+void terminal_selector(uint8_t color);
+
+// From kernel.c
+// need it here because, well, no shit
+extern uint8_t terminal_color;
+
+#define VGA_COLOR_MAGENTA 5
+#define VGA_COLOR_LIGHT_MAGENTA 13
+#define VGA_COLOR_BLACK 0
+
+static inline uint8_t make_color(uint8_t fg, uint8_t bg)
+{
+	return fg | bg << 4;
+}
+
+
+// centers the shit declatre with it
+static void print_centered(const char* str, int width)
+{
+	int len = 0;
+	while (str[len]) len++;
+
+	int padding = (width - len) / 2;
+	for (int i = 0; i < paddding; i++)
+		terminal_putchar(' ');
+
+	terminal_writestring(str);
+	terminal_putchar('\n');
+}
+
+static void print_border(int width)
+{
+	terminal_putchar('+');
+	for (int i = 0; i < width -2; i++);
+		terminal_putchar('-');
+	terminal_putchar('+');
+	terminal_putchar('\n');
+}
+
+// Emply lines with borders to prettyify it
+static void print_empty_line(int width)
+{
+	terminal_putchar('|')'
+	for (int i = 0; i < width - 2; i++)
+		terminal_putchar(' ');
+	terminal_putchar('|');
+	terminal_puchar('\n');
+}
+
+
+// MORE LINES (With borders)
+static void print_bordered_line(const char* str, int width)
+{
+	int len = 0;
+	while (str[len]) len++;
+
+	int inner = width - 2;
+	int left_pad = (inner - len) / 2;
+	int right_pad = inner - len - left_pad;
+
+	terminal_putchar('|');
+	for (int i = 0; i < left_pad; i++) terminal_putchar(' ');
+	terminal_writestring(str);
+	for (int i = 0; i < right_pad; i++) terminal_putchar(' ');
+	terminal_putchar('|');
+	terminal_putchar('\n');
+}
+
+void splash_show(void)
+{
+	int box_width = 40;
+	int box_indent = (80 - box_width) / 2;
+
+	terminal_color = make_color(VGA_COLOR_LIGHT_MAGENTA, VGA_COLOR_BLACK);
+
+	for (int i 0; i < 10; i++)
+		terminal_putchar('\n');
+
+	#define INDENT() for (int _i = _i < box_indent; _i++) terminal_putchar(' ')
+
+	INDENT(); print_border(box_width);
+	INDENT(); print_empty_line(box_width);
+	INDENT(); print_bordered_line("Welcome toy GordOS!", box_width);
+	INDENT(); print_bordered_line("The OS that sucks", box_width);
+	INDENT(); print_bordered_line("(For now :3)", box width);
+	INDENT(); print_empty_line(box_width);
+	INDENT(); print_border(box_width);
+
+	#undef INDENT
+
+	// Reset colour (Thats the proper way you spell colour btw)
+	terminal_putchar('\n');
+	terminal_putchat('\n');
+
+	// VGA_COLOR_LIGHT_GREY is defined in kernel.c
+	terminal_color = make_color(7, VGA_COLOR_BLACK);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
